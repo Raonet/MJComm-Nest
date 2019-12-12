e6e18b34eb6f46696ea24d2293c3c82ce4c0cdd5
Raonet <1027513492@qq.com>
2019-12-12 20:12:28 +0800

    添加user的增和删方法

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,8 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Param } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
-
+  constructor(private userService: UserService) {}
+  @Post('create')
+  async createUser(@Body() createUserDto: CreateUserDto): Promise<CreateUserDto> {
+    return this.userService.createUser(createUserDto);
+  }
+  @Post('del/:id')
+  async delUser(@Param()Params: CreateUserDto) {
+    return this.userService.delUser(Params.id);
+  }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,5 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
 import { Model } from 'mongoose';
+import { User } from './interfaces/user.interface';
+import { CreateUserDto } from './dto/create-user.dto';
 @Injectable()
 export class UserService {
+  constructor(@Inject('USER_MODEL') private readonly userModel: Model<User>) {}
+  // 创建用户
+  async createUser(createUserDto: CreateUserDto): Promise<User> {
+    const createUser = new this.userModel(createUserDto);
+    return await createUser.save();
+  }
+  // 删除用户
+  async delUser(userid) {
+    return this.userModel.remove({id: userid});
+  }
 }
